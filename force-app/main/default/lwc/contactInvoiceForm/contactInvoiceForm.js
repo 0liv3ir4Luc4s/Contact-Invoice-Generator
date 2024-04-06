c2ebaa4e4fd9c554cdf2671415ed0fa217fac62e
Lucas Oliveira Lima <0liv3ir4.luc4s@gmail.com>
2024-04-06 14:43:26 -0300

    feat: Add form and fields

--- force-app/main/default/lwc/contactInvoiceForm/contactInvoiceForm.js
+++ force-app/main/default/lwc/contactInvoiceForm/contactInvoiceForm.js
@@ -1,6 +1,12 @@
 import { LightningElement } from 'lwc';
+import EUR_PRICE from '@salesforce/schema/Invoice__c.BasePrice__c';
+import EXPIRATION_DATE from '@salesforce/schema/Invoice__c.ExpirationDate__c';
+import DESCRIPTION from '@salesforce/schema/Invoice__c.Description__c';
 
 export default class ContactInvoiceForm extends LightningElement {
+    EUR_PRICE_FIELD = EUR_PRICE;
+    EXPIRATION_DATE_FIELD = EXPIRATION_DATE;
+    DESCRIPTION_FIELD = DESCRIPTION;
 
-    
+    objectApiName = 'Invoice__c';
 }
